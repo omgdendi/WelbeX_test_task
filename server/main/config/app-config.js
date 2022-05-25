bb220508a4ac83f51bdd1c4607cf9e2516f5b388
omgdendi <a4baytakov@mail.ru>
2022-05-25 21:19:06 +0300

    allow cors

--- server/main/config/app-config.js
+++ server/main/config/app-config.js
@@ -6,18 +6,13 @@ const express = require("express");
 const path = require("path");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
-const blogRouter = require("../routers/blog-router");
-const authRouter = require("../routers/auth-router");
 const errorMiddleware = require("../middleware/error-middleware");
 const router = require("../routers/base-router");
 
 module.exports = appConfig = (app) => {
     app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
     app.use(bodyParser.json())
-    app.use(cors({
-        credentials: true,
-        origin: process.env.CLIENT_URL
-    }));
+    app.use(cors());
     app.use(express.static(path.resolve("main", 'static/images')));
     app.use(express.static(path.resolve("main", 'static/video')));
     app.use(fileUpload({}));
